0c976c43cfe8da6cff174412d20f4a9764ebe77b
Muhammad Faizan Abbasi <faizanx168@gmail.com>
2022-12-21 13:48:25 -0500

    changed password settings

--- controllers/users.js
+++ controllers/users.js
@@ -34,7 +34,7 @@ exports.registerUser = asyncError(async (req, res) => {
       const token = jwt.sign(
         { username, email, password, firstName, lastName },
         process.env.JWT_ACTIVATE,
-        { expiresIn: "5m" }
+        { expiresIn: "15m" }
       );
 
       subject = "Tarpit Account Activation Link";
@@ -51,7 +51,7 @@ exports.registerUser = asyncError(async (req, res) => {
       if (sent) {
         req.flash(
           "success",
-          "Email has been sent! Kindly activate your account. Token expires in 5 mins!"
+          "Email has been sent! Kindly activate your account. Token expires in 15 mins!"
         );
         res.redirect("/login");
       } else {
